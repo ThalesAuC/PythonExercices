93069cfb8dea5833ff63ff4ae5f3448f471e52f2
ThunderShout <thalesaugusto312@gmail.com>
2022-02-15 17:37:06 -0300

    update

--- Python/dicionarioDB.py
+++ Python/dicionarioDB.py
@@ -2,11 +2,12 @@ from distutils.log import info
 
 banco={}
 i = 0
-while i < 10:
+while i < 2:
     
     name = input("insira o nome para o cadastro: ")
     info = input("insira uma informação para o banco: ")
-    banco[name] = info
+    info2 = input("insira uma segunda informação: ")
+    banco[name] = [info, info2]
     i+=1
 
 print(banco)
